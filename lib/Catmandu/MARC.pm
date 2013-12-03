49dcdbf8464bc4c3f9d42fc90d9b65bbea1c5ca1
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2013-12-03 10:45:47 +0100

    bump version

--- lib/Catmandu/MARC.pm
+++ lib/Catmandu/MARC.pm
@@ -6,11 +6,11 @@ Catmandu::MARC - Catmandu modules for working with MARC data
 
 =head1 VERSION
 
-Version 0.08
+Version 0.1
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.1';
 
 =head1 MODULES
 
