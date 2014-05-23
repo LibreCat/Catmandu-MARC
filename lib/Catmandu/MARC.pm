ec3ff22579b2f08342a4c71e8680a6abd53fcec2
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2014-05-23 10:12:12 +0200

    Adding more documentation hints

--- lib/Catmandu/MARC.pm
+++ lib/Catmandu/MARC.pm
@@ -16,9 +16,21 @@ our $VERSION = '0.115';
 
 =item * L<Catmandu::Fix::marc_map>
 
+=item * L<Catmandu::Fix::marc_add>
+
+=item * L<Catmandu::Fix::marc_remove>
+
 =item * L<Catmandu::Fix::marc_xml>
 
 =item * L<Catmandu::Fix::marc_in_json>
+
+=item * L<Catmandu::Fix::Condition::marc_match>
+
+=item * L<Catmandu::Fix::Inline::marc_map>
+
+=item * L<Catmandu::Fix::Inline::marc_add>
+
+=item * L<Catmandu::Fix::Inline::marc_remove>
 
 =back
 
