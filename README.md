3ad65d09158a841f971ad87e8e8e2e6df0fb33b3
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2014-05-23 12:05:00 +0200

    Fixing buggy implementation

--- README.md
+++ README.md
@@ -6,8 +6,14 @@ Catmandu::MARC - Catmandu modules for working with MARC data
 
 - [Catmandu::Importer::MARC](https://metacpan.org/pod/Catmandu::Importer::MARC)
 - [Catmandu::Fix::marc\_map](https://metacpan.org/pod/Catmandu::Fix::marc_map)
+- [Catmandu::Fix::marc\_add](https://metacpan.org/pod/Catmandu::Fix::marc_add)
+- [Catmandu::Fix::marc\_remove](https://metacpan.org/pod/Catmandu::Fix::marc_remove)
 - [Catmandu::Fix::marc\_xml](https://metacpan.org/pod/Catmandu::Fix::marc_xml)
 - [Catmandu::Fix::marc\_in\_json](https://metacpan.org/pod/Catmandu::Fix::marc_in_json)
+- [Catmandu::Fix::Condition::marc\_match](https://metacpan.org/pod/Catmandu::Fix::Condition::marc_match)
+- [Catmandu::Fix::Inline::marc\_map](https://metacpan.org/pod/Catmandu::Fix::Inline::marc_map)
+- [Catmandu::Fix::Inline::marc\_add](https://metacpan.org/pod/Catmandu::Fix::Inline::marc_add)
+- [Catmandu::Fix::Inline::marc\_remove](https://metacpan.org/pod/Catmandu::Fix::Inline::marc_remove)
 
 # AUTHOR
 
--- lib/Catmandu/Fix/Condition/marc_match.pm
+++ lib/Catmandu/Fix/Condition/marc_match.pm
@@ -13,23 +13,26 @@ has marc_path  => (fix_arg => 1);
 has value      => (fix_arg => 1);
 
 sub emit {
-	my ($self,$fixer,$label) = @_;
+    my ($self,$fixer,$label) = @_;
 
-	my $perl;
+    my $perl;
 
-	my $tmp_var  = '_tmp_' . int(rand(9999));
-	my $marc_map = Catmandu::Fix::marc_map->new($self->marc_path , "$tmp_var.\$append");
-	$perl .= $marc_map->emit($fixer,$label);
+    my $tmp_var  = '_tmp_' . int(rand(9999));
+    my $marc_map = Catmandu::Fix::marc_map->new($self->marc_path , "$tmp_var.\$append");
+    $perl .= $marc_map->emit($fixer,$label);
 
-	my $all_match = Catmandu::Fix::Condition::all_match->new("$tmp_var.*",$self->value);
-	$all_match->pass_fixes($self->pass_fixes);
-	$all_match->fail_fixes($self->fail_fixes);
-	$perl .= $all_match->emit($fixer,$label);  
+    my $all_match = Catmandu::Fix::Condition::all_match->new("$tmp_var.*",$self->value);
+    my $remove_field = Catmandu::Fix::remove_field->new($tmp_var);
 
-	my $remove_field = Catmandu::Fix::remove_field->new($tmp_var);
-	$perl .= $remove_field->emit($fixer,$label);
+    my $pass_fixes = $self->pass_fixes;
+    my $fail_fixes = $self->fail_fixes;
 
-	$perl;
+    $all_match->pass_fixes([ $remove_field , @$pass_fixes ]);
+    $all_match->fail_fixes([ $remove_field , @$fail_fixes ]);
+
+    $perl .= $all_match->emit($fixer,$label);
+
+    $perl;
 }
 
 =head1 NAME
